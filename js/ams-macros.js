fc02117bba2db953a82ad2d94621b49bab89c0f8
Peter Krautzberger <p.krautzberger@gmail.com>
2023-08-11 12:45:04 +0200

    chore: regenerate build

--- js/ams-macros.js
+++ js/ams-macros.js
@@ -1,4 +1,3 @@
-"use strict";
 /*!
  *  Copyright (c) 2020 American Mathematical Society
  *
@@ -14,78 +13,54 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-var __values = (this && this.__values) || function(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-    if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
-        next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configuration = void 0;
-var NodeUtil_js_1 = require("mathjax-full/js/input/tex/NodeUtil.js");
-var Configuration_js_1 = require("mathjax-full/js/input/tex/Configuration.js");
-var SymbolMap_js_1 = require("mathjax-full/js/input/tex/SymbolMap.js");
-var TexConstants_js_1 = require("mathjax-full/js/input/tex/TexConstants.js");
-var ParseMethods_js_1 = require("mathjax-full/js/input/tex/ParseMethods.js");
-var BaseMethods_js_1 = require("mathjax-full/js/input/tex/base/BaseMethods.js");
-var AmsMacrosMethods = {};
+import NodeUtil from 'mathjax-full/js/input/tex/NodeUtil.js';
+import { Configuration } from 'mathjax-full/js/input/tex/Configuration.js';
+import { CommandMap, DelimiterMap } from 'mathjax-full/js/input/tex/SymbolMap.js';
+import { TexConstant } from 'mathjax-full/js/input/tex/TexConstants.js';
+import ParseMethods from 'mathjax-full/js/input/tex/ParseMethods.js';
+import BaseMethods from 'mathjax-full/js/input/tex/base/BaseMethods.js';
+let AmsMacrosMethods = {};
 AmsMacrosMethods.accentset = function (parser, name) {
-    var accent = parser.ParseArg(name);
-    var expression = parser.ParseArg(name);
+    const accent = parser.ParseArg(name);
+    const expression = parser.ParseArg(name);
     if (accent.isKind('mo'))
-        NodeUtil_js_1.default.setAttribute(accent, 'accent', true);
-    NodeUtil_js_1.default.setAttribute(accent, 'mathsize', 'small');
+        NodeUtil.setAttribute(accent, 'accent', true);
+    NodeUtil.setAttribute(accent, 'mathsize', 'small');
     if (parser.stack.env.font) {
-        NodeUtil_js_1.default.setAttribute(accent, 'mathvariant', parser.stack.env.font);
+        NodeUtil.setAttribute(accent, 'mathvariant', parser.stack.env.font);
     }
-    var mpadded = parser.create('node', 'mpadded', [accent], { height: 0 });
-    var mover = parser.create('node', 'mover', [expression, mpadded]);
-    var texatom = parser.create('node', 'TeXAtom', [mover]);
+    const mpadded = parser.create('node', 'mpadded', [accent], { height: 0 });
+    const mover = parser.create('node', 'mover', [expression, mpadded]);
+    const texatom = parser.create('node', 'TeXAtom', [mover]);
     parser.Push(texatom);
 };
 AmsMacrosMethods.smallcaps = function (parser, name) {
-    var e_1, _a;
-    var argument = parser.GetArgument(name);
-    var def = {
+    let argument = parser.GetArgument(name);
+    const def = {
         mathsize: '1em',
         mathvariant: 'normal',
     };
-    try {
-        for (var argument_1 = __values(argument), argument_1_1 = argument_1.next(); !argument_1_1.done; argument_1_1 = argument_1.next()) {
-            var char = argument_1_1.value;
-            if (char.toLowerCase() === char) {
-                def.mathsize = '0.8em';
-            }
-            else
-                def.mathsize = '1em';
-            parser.Push(parser.create('token', 'mi', def, char.toUpperCase()));
+    for (let char of argument) {
+        if (char.toLowerCase() === char) {
+            def.mathsize = '0.8em';
         }
-    }
-    catch (e_1_1) { e_1 = { error: e_1_1 }; }
-    finally {
-        try {
-            if (argument_1_1 && !argument_1_1.done && (_a = argument_1.return)) _a.call(argument_1);
-        }
-        finally { if (e_1) throw e_1.error; }
+        else
+            def.mathsize = '1em';
+        parser.Push(parser.create('token', 'mi', def, char.toUpperCase()));
     }
 };
 AmsMacrosMethods.bevelledFraction = function (parser, name) {
-    var num = parser.ParseArg(name);
-    var den = parser.ParseArg(name);
-    var frac = parser.create('node', 'mfrac', [num, den], {
+    const num = parser.ParseArg(name);
+    const den = parser.ParseArg(name);
+    const frac = parser.create('node', 'mfrac', [num, den], {
         bevelled: true,
     });
     parser.Push(frac);
 };
-AmsMacrosMethods.Macro = BaseMethods_js_1.default.Macro;
-AmsMacrosMethods.Accent = BaseMethods_js_1.default.Accent;
-AmsMacrosMethods.SetFont = BaseMethods_js_1.default.SetFont;
-new SymbolMap_js_1.CommandMap('ams-macros-macros', {
+AmsMacrosMethods.Macro = BaseMethods.Macro;
+AmsMacrosMethods.Accent = BaseMethods.Accent;
+AmsMacrosMethods.SetFont = BaseMethods.SetFont;
+new CommandMap('ams-macros-macros', {
     pmod: [
         'Macro',
         '\\mathchoice{\\mkern18mu}{\\mkern12mu}{\\mkern12mu}{\\mkern12mu}(\\operatorname{mod}\\mkern6mu #1)',
@@ -104,7 +79,7 @@ new SymbolMap_js_1.CommandMap('ams-macros-macros', {
     widecheck: ['Accent', '02C7', 1],
     accentset: 'accentset',
     intbar: ['Macro', '\\mmlToken{mo}{\u2a0d}'],
-    bfit: ['SetFont', TexConstants_js_1.TexConstant.Variant.BOLDITALIC],
+    bfit: ['SetFont', TexConstant.Variant.BOLDITALIC],
     mathbfit: ['Macro', '{\\bfit #1}', 1],
     mathsc: 'smallcaps',
     mathbfcal: ['Macro', '\\boldsymbol{\\mathcal{#1}}', 1],
@@ -114,7 +89,7 @@ new SymbolMap_js_1.CommandMap('ams-macros-macros', {
     De: ['Macro', '\\mathrm{\u0434}'],
     txt: ['Macro', '\\vcenter{\\makebox{#1}}', 1],
 }, AmsMacrosMethods);
-new SymbolMap_js_1.DelimiterMap('ams-macros-delimiters', ParseMethods_js_1.default.delimiter, {
+new DelimiterMap('ams-macros-delimiters', ParseMethods.delimiter, {
     '\\Vvert': '\u2980',
     '\\llbracket': '\u27E6',
     '\\rrbracket': '\u27E7',
@@ -123,7 +98,7 @@ new SymbolMap_js_1.DelimiterMap('ams-macros-delimiters', ParseMethods_js_1.defau
     '\\lAngle': '\u27EA',
     '\\rAngle': '\u27EB',
 });
-exports.configuration = Configuration_js_1.Configuration.create('ams-macros', {
+export const configuration = Configuration.create('ams-macros', {
     handler: {
         delimiter: ['ams-macros-delimiters'],
         macro: ['ams-macros-macros', 'ams-macros-delimiters'],
